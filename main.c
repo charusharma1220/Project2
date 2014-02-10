6c88d0698a51961c408b7b28ad9e37608f8b2676
DWDenton <dwdenton06@gmail.com>
2014-02-10 14:50:46 -0600

    added debug folder.

--- main.c
+++ main.c
@@ -13,10 +13,6 @@ void f3(void *arg)
 void f2(void *arg)
 {
     int i = 0;
-    struct thread *t2 = thread_create(f2, NULL);
-    thread_add_runqueue(t2);
-    struct thread *t3 = thread_create(f3, NULL);
-    thread_add_runqueue(t3);
     while(1) {
         printf("thread 2: %d\n",i++);
         if (i == 10) {
@@ -46,14 +42,7 @@ int main(int argc, char **argv)
 {
     struct thread *t1 = thread_create(f1, NULL);
     thread_add_runqueue(t1);
-    struct thread *t2 = thread_create(f1, NULL);
-    thread_add_runqueue(t2);
-    struct thread *t3 = thread_create(f1, NULL);
-    thread_add_runqueue(t3);
-
     thread_start_threading();
-
-    thread_start_threading();     //DEBUG: corner case
     printf("\nexited\n");
     return 0;
 }

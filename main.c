c2ba5137d59204ab974abdd32093fbcfdd528a48
charusharma1220 <charu.sharma@utexas.edu>
2014-02-10 23:09:35 -0600

    Update main.c
    I think there'a bug in the main code he posted at first, I added the initialization in f3

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 void f3(void *arg)
 {
-    int i;
+    int i = 0;
     while (1) {
         printf("thread 3: %d\n", i++);
         thread_yield();
@@ -19,8 +19,8 @@ void f2(void *arg)
             i = 0;
         }
         thread_yield();
+        printf("came from a long jump\n");
     }
-}
 
 void f1(void *arg)
 {
@@ -35,13 +35,17 @@ void f1(void *arg)
             i = 100;
         }
         thread_yield();
+        printf("came from a long jump\n");
     }
 }
 
 int main(int argc, char **argv)
 {
+	printf("in main\n");
     struct thread *t1 = thread_create(f1, NULL);
+    printf("thread t1 created\n");
     thread_add_runqueue(t1);
+    printf("thread t1 added to queue\n");
     thread_start_threading();
     printf("\nexited\n");
     return 0;
